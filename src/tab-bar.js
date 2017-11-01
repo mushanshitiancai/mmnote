61cdad8d46032168afac1a35e1d8eb4af0e1fdaf
mazhibin <mazhibin@ppzuche.com>
2017-11-01 23:18:19 +0800

    update tab-bar.js

--- src/tab-bar.js
+++ src/tab-bar.js
@@ -9,14 +9,14 @@ class TabBar extends EventEmitter {
         super();
         window._d.tabs = this;
 
-        this.model = model
-        this.$container = $container
-        this.tabMap = {}
-        this.tabIds = [];
-        this.activeTabId = null
+        this._model = model
+        this._$container = $container
+        this._tabMap = {}
+        this._tabIds = [];
+        this._activeTabId = null
 
         model.on(Model.EVENTS.openNote, (note) => {
-            this.addTab(note.uri.toString(), note.name, null);
+            this.appendTab(new Tab(note.uri.toString(), note.name));
         });
 
         model.on(Model.EVENTS.activeNote, (note) => {
@@ -28,48 +28,90 @@ class TabBar extends EventEmitter {
         });
     }
 
-    addTab(tab, index){
-        if(index > this.tabIds.length){
-            index = this.tabIds.length
+    appendTab(tab) {
+        let newTabId = tab.getId();
+
+        if (this._tabMap[newTabId]) {
+            return this.activeTab(newTabId);
         }
 
-        _
+        this._$container.append(tab.getElement());
+
+        this._tabMap[newTabId] = tab;
+        this._tabIds.push(newTabId);
+
+        this.activeTab(newTabId);
+        return this._tabIds.length - 1;
+    }
+
+    activeTab(id){
+        
     }
 }
 
-class Tab extends EventEmitter{
+class Tab extends EventEmitter {
     constructor(id, title) {
-        this.id = id;
-        this.title = title;
+        that = this;
+        this._id = id;
+        this._title = title;
+        this._isDirty = false;
 
-        this.$tab = $('<div>', {
+        this._$tab = $('<div>', {
             class: "tab",
             click: function () {
-                model.activeNote(id);
+                that.emit(Tab.EVENTS.click, that);
             }
         });
 
         let $close = $('<span>', {
             class: "icon icon-cancel icon-close-tab",
             click: function () {
-                let tabId = $(this).parent('.tab-item').data("id");
-                model.closeNote(tabId);
+                that.emit(Tab.EVENTS.close, that);
             }
-        }).appendTo(this.$tab);
-        
+        }).appendTo(this._$tab);
+
         let $title = $("<span>", {
             class: "title",
             text: title
-        }).appendTo(this.$tab);
+        }).appendTo(this._$tab);
+
+        this.$dirtyMark = $("<span>", {
+            text: "*",
+            css: {
+                display: "none"
+            }
+        }).appendTo($title);
     }
 
-    getId(){
-        return this.id;
+    getElement() {
+        return this._$tab;
     }
 
-    getTitle(){
-        return this.title;
+    getId() {
+        return this._id;
     }
+
+    getTitle() {
+        return this._title;
+    }
+
+    isDirty() {
+        return this._isDirty;
+    }
+
+    setDirty(isDirty) {
+        this._isDirty = !!isDirty;
+        if (this._isDirty) {
+            this.$dirtyMark.css("display", "normal")
+        } else {
+            this.$dirtyMark.css("display", "none")
+        }
+    }
+}
+
+Tab.EVENTS = {
+    click: "click",
+    close: "close"
 }
 
 module.exports = TabBar
